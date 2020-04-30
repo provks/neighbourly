950ec6fd78e198439cc2cca7e6c3d5148be62446
[varun <[coldvks@gmail.com]>
2020-05-01 01:14:35 +0530

    added views and implemented paritals

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 const usersController = require('../controllers/users_controller');
 
-console.log(`Router Index is loaded!`);
+// console.log(`Router Index is loaded!`);
 
 router.get('/', homeController.home);
 router.get('/users', usersController.profile);
